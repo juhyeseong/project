c7eae6ccd8421fbea1bad7bdc65d93625a6f675a
KyungsooJ <49545509+KyungsooJ@users.noreply.github.com>
2023-08-11 18:16:48 +0900

    Delete day01 directory

--- day01/test2/src/main/java/toy/AA.java
+++
@@ -1,9 +0,0 @@
-package toy;
-
-public class AA implements Battery {
-
-	@Override
-	public String toString() {
-		return "AA";
-	}
-}
--- day01/test2/src/main/java/toy/Battery.java
+++
@@ -1,5 +0,0 @@
-package toy;
-
-public interface Battery {
-
-}
