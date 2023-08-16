8ff37d8ca193d5913d1b57fc788d3571cfb8c097
HongMinha <hlkm1667haha@gmail.com>
2023-08-16 17:08:11 +0900

    회원가입, 로그인 기능 구현

---
+++ dbjava/src/main/java/com/javabang/controller/MemberController.java
@@ -0,0 +1,51 @@
+package com.javabang.controller;
+
+
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javabang.model.MemberDTO;
+import com.javabang.service.MemberService;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@Autowired private MemberService mservice;
+	
+	//회원가입
+	@GetMapping("/join")
+	public void join() {}
+	
+	@PostMapping("/join")
+	public String join(MemberDTO dto) throws NoSuchAlgorithmException  {
+		int row = mservice.add(dto);
+		System.out.println(row + "행이 추가 되었습니다");
+		return "redirect:/member/login";
+	}
+	
+	//로그인
+	@GetMapping("/login")
+	public void login() {}
+	@PostMapping("/login")
+	public String login(MemberDTO dto, HttpSession session) throws NoSuchAlgorithmException  {
+		MemberDTO login = mservice.login(dto); 
+		session.setAttribute("login", login);
+		return "redirect:/";
+	}
+	
+	//로그아웃
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+}
---
+++ dbjava/src/main/java/com/javabang/model/MemberDTO.java
@@ -0,0 +1,101 @@
+package com.javabang.model;
+
+import java.sql.Date;
+
+public class MemberDTO {
+	private int idx;
+	private String userId;
+	private String userPw;
+	private String userNick;
+	private String email;
+	private String year;
+	private String month;
+	private String day;
+	private String userName;
+	private String phoneNum;
+	private String profile;
+	private String gender;
+
+	private Date birth;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserNick() {
+		return userNick;
+	}
+	public void setUserNick(String userNick) {
+		this.userNick = userNick;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
